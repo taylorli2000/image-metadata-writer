3f00bd7423b3b27a4e4178dad8a155423a62e34b
Taylor Li <t4ylor.li@gmail.com>
2021-11-08 09:29:58 -0500

    text image recognition with pytesseract

--- server/app/images.py
+++ server/app/images.py
@@ -1,16 +1,19 @@
 import os
-from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 from exif import Image
 import reverse_geocoder as rg
 import pycountry
 import zipfile
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd='C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 from flask import (
     Blueprint, g, request, session, current_app, send_from_directory, send_file, jsonify, make_response
 )
 
-from app.db import get_db
+from app.db import get_db, close_db
 
 bp = Blueprint('images', __name__, url_prefix='/images')
 
@@ -19,8 +22,9 @@ bp = Blueprint('images', __name__, url_prefix='/images')
 def images():
     if request.method == "GET":
         db = get_db()
-        zipfolder = zipfile.ZipFile('photos.zip','w', compression = zipfile.ZIP_STORED)
         photos = db.execute("SELECT photo_path, photo_name, photo_description FROM photos").fetchall()
+        close_db()
+        zipfolder = zipfile.ZipFile('photos.zip','w', compression = zipfile.ZIP_STORED)
         for photo in photos:
             zipfolder.write(photo[0])
         zipfolder.close()
@@ -32,9 +36,11 @@ def images():
         for file in request.files:
             filename = secure_filename(request.files[file].filename)
             request.files[file].save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            img = cv2.imread(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            img_text = pytesseract.image_to_string(img)
             with open(current_app.config['UPLOAD_FOLDER'] + filename, "rb") as photo_file:
                 photo_image = Image(photo_file)
-                if photo_image.has_exif == False:
+                if photo_image.has_exif != True:
                     photo_image.image_description = 'unknown'
                 else:
                     decimal_latitude = dms_coordinates_to_dd_coordinates(photo_image.get('gps_latitude','unknown'), photo_image.get('gps_latitude_ref', 'unknown'))
@@ -45,16 +51,13 @@ def images():
                         location_info['country'] = pycountry.countries.get(alpha_2=location_info['cc'])
                         photo_image.image_description = f'Location: {location_info.get("name")}, {location_info.get("admin1")}'
                 db.execute("INSERT INTO photos (photo_path, photo_name, photo_description) VALUES (?, ?, ?)", 
-                            (os.path.join(current_app.config['UPLOAD_FOLDER'], filename), filename, photo_image.get('image_description', 'unknown'))
+                            (os.path.join(current_app.config['UPLOAD_FOLDER'], filename), filename, img_text + photo_image.get('image_description', 'unknown'))
                             )
                 db.commit()
-
                 with open(current_app.config['UPLOAD_FOLDER'] + filename, "wb") as photo_file_updated:
                     photo_file_updated.write(photo_image.get_file())
-                
+        close_db()
         return ('', 200)
-
-
     return ('', 500)
 
 def dms_coordinates_to_dd_coordinates(coordinates, coordinates_ref):
